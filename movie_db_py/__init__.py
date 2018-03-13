f044b76f091f0ec8cb1776cdea6a35e1db5fbefb
vishvAsaH <vishvas.vasuki@gmail.com>
2018-03-12 20:27:38 -0700

    Successful test?

--- movie_db_py/__init__.py
+++ movie_db_py/__init__.py
@@ -1,7 +1,27 @@
+import json
+import tmdbsimple as tmdb
+import logging
+
+logging.basicConfig(
+  level=logging.DEBUG,
+  format="%(levelname)s: %(asctime)s %(message)s"
+)
 
 
-def get_years():
-  pass
+local_config = json.load(open('local_config.json'))
+tmdb.API_KEY = local_config["tmdb_api_key"]
+
+
+def get_release_date(query):
+  response = tmdb.Search().movie(query = query)
+  if len(response) == 1:
+    return response.release_date
+  else:
+    return None
+
+
+
+
 
 if __name__ == '__main__':
-  get_years()
+  logging.debug(get_release_date(query = "Bourne"))
